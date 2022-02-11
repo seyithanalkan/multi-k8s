5e11af491b250f87e0a6eae00905d80112252108
seyit han alkan <seyithanalkan@yahoo.com>
2022-02-11 21:25:22 +0300

    last-2

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t sythnlkn/multi-client:latest -t sythnlkn/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t sythnlkn/multi-server:latest -t sythnlkn/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t sythnlkn/multi-worker:latest -t sythnlkn/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push sythnlkn/multi-client:latest
-docker push sythnlkn/multi-server:latest
-docker push sythnlkn/multi-worker:latest
+docker build -t sythnlkn/multi-client-k8s:latest -t sythnlkn/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t sythnlkn/multi-server-k8s-pgfix:latest -t sythnlkn/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
+docker build -t sythnlkn/multi-worker-k8s:latest -t sythnlkn/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
 
-docker push sythnlkn/multi-client:$SHA
-docker push sythnlkn/multi-server:$SHA
-docker push sythnlkn/multi-worker:$SHA
+docker push sythnlkn/multi-client-k8s:latest
+docker push sythnlkn/multi-server-k8s-pgfix:latest
+docker push sythnlkn/multi-worker-k8s:latest
 
+docker push sythnlkn/multi-client-k8s:$SHA
+docker push sythnlkn/multi-server-k8s-pgfix:$SHA
+docker push sythnlkn/multi-worker-k8s:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=sythnlkn/multi-server:$SHA
-kubectl set image deployments/client-deployment server=sythnlkn/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=sythnlkn/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=sythnlkn/multi-server-k8s-pgfix:$SHA
+kubectl set image deployments/client-deployment client=sythnlkn/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=sythnlkn/multi-worker-k8s:$SHA
