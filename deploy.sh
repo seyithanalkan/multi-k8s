40ea654a36723e8a3e6aadb463f7a4b3dbc9843b
seyit han alkan <seyithanalkan@yahoo.com>
2022-02-12 01:40:33 +0300

    ltst2

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t cygnetops/multi-client-k8s:latest -t cygnetops/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
-docker build -t cygnetops/multi-server-k8s-pgfix:latest -t cygnetops/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
-docker build -t cygnetops/multi-worker-k8s:latest -t cygnetops/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+docker build -t sythnlkn/multi-client:latest -t sythnlkn/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t sythnlkn/multi-server:latest -t sythnlkn/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t sythnlkn/multi-worker:latest -t sythnlkn/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push cygnetops/multi-client-k8s:latest
-docker push cygnetops/multi-server-k8s-pgfix:latest
-docker push cygnetops/multi-worker-k8s:latest
+docker push sythnlkn/multi-client:latest
+docker push sythnlkn/multi-server:latest
+docker push sythnlkn/multi-worker:latest
 
-docker push cygnetops/multi-client-k8s:$SHA
-docker push cygnetops/multi-server-k8s-pgfix:$SHA
-docker push cygnetops/multi-worker-k8s:$SHA
+docker push sythnlkn/multi-client:$SHA
+docker push sythnlkn/multi-server:$SHA
+docker push sythnlkn/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=cygnetops/multi-server-k8s-pgfix:$SHA
-kubectl set image deployments/client-deployment client=cygnetops/multi-client-k8s:$SHA
-kubectl set image deployments/worker-deployment worker=cygnetops/multi-worker-k8s:$SHA
+kubectl set image deployments/server-deployment server=sythnlkn/multi-server:$SHA
+kubectl set image deployments/client-deployment client=sythnlkn/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=sythnlkn/multi-worker:$SHA
